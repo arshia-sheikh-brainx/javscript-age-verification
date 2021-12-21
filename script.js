56a309dc8f8a33be532fc5195f40fc46ba46846a
Arshia Sheikh <95319130+arshia-sheikh-brainx@users.noreply.github.com>
2021-12-21 19:57:17 +0500

    remove year selection error

--- script.js
+++ script.js
@@ -1,42 +1,46 @@
 //get the form and add event lister to the form 
 
 let formsub = document.getElementById("age");
-formsub.addEventListener("submit",(e)=>{
+formsub.addEventListener("submit", (e) => {
     e.preventDefault();
     verifyAge();
 
 });
 // function for age verification
-function verifyAge(){
+function verifyAge() {
     // get user input values
-   let dayvalue= Number(document.getElementById("day").value);
-   let monthvalue=Number(document.getElementById("month").value)-1;
-   let yearvalue= Number(document.getElementById("year").value);
-//    current date 
-   let date=new Date();
-   let currentDay=date.getDate();
-   let currentMonth=date.getMonth();
-   let currentYear=date.getFullYear();
-//    difference betweeb current year and user input year
-   let difference=currentYear-yearvalue;
-//    checks for the age verification
-   if(difference> 15){
-       if(currentMonth>monthvalue){
-           window.location.assign("https://www.google.com/");
-       }else if (currentMonth==monthvalue){
-           if(currentDay>=dayvalue){
-           window.location.assign("https://www.google.com/");
-           }
-           else{
-            alert("Your age is restricted");
+    let dayvalue = Number(document.getElementById("day").value);
+    let monthvalue = Number(document.getElementById("month").value) - 1;
+    let yearvalue = Number(document.getElementById("year").value);
+    //    current date 
+    let date = new Date();
+    let currentDay = date.getDate();
+    let currentMonth = date.getMonth();
+    let currentYear = date.getFullYear();
+    //    difference betweeb current year and user input year
+    let difference = currentYear - yearvalue;
+
+    //    checks for the age verification
+    if (difference > 15) {
+        window.location.assign("https://www.google.com/");
+    }
+    else if (difference === 15) {
+        if (currentMonth > monthvalue) {
+            window.location.assign("https://www.google.com/");
+        } else if (currentMonth == monthvalue) {
+            if (currentDay >= dayvalue) {
+                window.location.assign("https://www.google.com/");
+            }
+            else {
+                alert("Your age is restricted");
             }
 
-       }else{
-       alert("Your age is restricted");
-       }
+        } else {
+            alert("Your age is restricted");
+        }
 
-   }else{
-       alert("Your age is restricted");
-   }
-   
+    } else {
+        alert("Your age is restricted");
+    }
+
 }
